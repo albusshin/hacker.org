17f36c3bf65d366b53f8ea9e10ab3f6b533bca73
albusshin <albusshin@gmail.com>
2013-05-21 19:16:24 +1000

    Biggest Fib

---
+++ Biggest Fib/solve.py
@@ -0,0 +1,11 @@
+'''
+Created on 2013-5-21
+
+@author: Administrator
+'''
+
+
+import math
+
+x = ((150000000000000) - math.log(math.sqrt(5), (math.sqrt(5) + 1)/2))/math.log(math.e, (math.sqrt(5) + 1)/2)
+print repr(x)
